dc7ef2ce5b56ec7eafa9332a51da7e358d2b516f
Dawid Bańczak <dawid.banczak99@gmail.com>
2022-08-15 18:46:41 +0200

    Testing angular functionality on products component

--- src/app/products.component.ts
+++ src/app/products.component.ts
@@ -4,4 +4,16 @@ import { Component } from "@angular/core";
     selector: 'app-products',
     templateUrl: './products.component.html'    
 })
-export class ProductsComponent {}
+export class ProductsComponent {
+    productName = 'A book';
+
+    constructor() {
+        setTimeout(() => {
+            this.productName = 'A tree';
+        }, 3000);
+    }
+
+    ChangeName() {
+        this.productName = 'A Boat';
+    }
+}
