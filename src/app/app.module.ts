e9994ff3115b1976e2c7d2dbfe74001392f2a08b
Dawid Bańczak <dawid.banczak99@gmail.com>
2022-08-23 00:56:42 +0200

    Added service and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,22 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './products.component';
 import { ProductComponent } from './product/product.component';
+import { ProductsService } from './products.service';
+import { HomeComponent } from './home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductsComponent,
-    ProductComponent
+    ProductComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule
   ],
-  providers: [],
+  providers: [ProductsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/products.component.ts
+++ src/app/products.component.ts
@@ -1,26 +1,43 @@
-import { Component } from "@angular/core";
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ProductsService } from './products.service';
+import { Subscription } from 'rxjs';
 
 @Component({
-    selector: 'app-products',
-    templateUrl: './products.component.html'    
+  selector: 'app-products',
+  templateUrl: './products.component.html',
 })
-export class ProductsComponent {
-    productName = 'A book';
-    products: string[] = [];
-    isDisabled = true;
-    constructor() {
-        setTimeout(() => {
-            this.isDisabled = false;
-        }, 1000);
-    }
+export class ProductsComponent implements OnInit, OnDestroy {
+  productName = 'A book';
+  isDisabled = true;
+  products: string[] = [];
+  private productSubscription: Subscription;
 
-    onAddProduct(form : any) {
-            if(form.status === "VALID"){
-                this.products.push(form.value.productName);
-            }
-    }
+  constructor(private productsService: ProductsService) {
+    setTimeout(() => {
+      this.isDisabled = false;
+    }, 1000);
+  }
 
-    onRemoveProduct(product: string){
-        this.products = this.products.filter(x => x !== product);
+  ngOnInit(): void {
+    this.products = this.productsService.getProducts();
+    this.productSubscription = this.productsService.productsUpdated.subscribe(
+      () => {
+        this.products = this.productsService.getProducts();
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.productSubscription.unsubscribe();
+  }
+
+  onAddProduct(form: any) {
+    if (form.status === 'VALID') {
+      this.productsService.addProduct(form.value.productName);
     }
+  }
+
+  onRemoveProduct(product: string) {
+    this.products = this.products.filter((x) => x !== product);
+  }
 }
