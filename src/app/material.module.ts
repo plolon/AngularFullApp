c42d43ee0ba1e5556c0295f2348bbef627c47638
Dawid Bańczak <dawid.banczak99@gmail.com>
2022-08-23 13:27:30 +0200

    Created mat icon button

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { MatSliderModule } from '@angular/material/slider';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-    imports: [],
-    exports: [MatSliderModule]
+  imports: [],
+  exports: [MatSliderModule, MatButtonModule, MatIconModule],
 })
 export class MaterialModule {}
