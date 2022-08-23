92cc2b46a6398bcc257eee2e1c8c147abed71655
Dawid Bańczak <dawid.banczak99@gmail.com>
2022-08-23 17:55:00 +0200

    Created email and password input labels with angular material

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   imports: [],
   exports: [
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule
 ],
 })
 export class MaterialModule {}
